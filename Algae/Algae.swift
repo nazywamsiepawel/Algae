d112b38f316db73279f4233f1251e23fae29245a
Pawel Borkowski <borkowskip@gmail.com>
2016-05-22 09:05:37 +0100

    Move out GPU stuff for later.

--- Algae/Algae.swift
+++ Algae/Algae.swift
@@ -9,23 +9,6 @@
 import Foundation
 
 public class Algae {
-    init(){
-//        let bundle = NSBundle(forClass: self.dynamicType)
-//        let path = bundle.pathForResource("FOCUS", ofType: "png")!
-//        let xmlData = NSData(contentsOfFile: path)
-//        print(xmlData)
-//        print("")
-//        let path = bundle.pathForResource("Shaders", ofType: "metal")
-//        do {
-//            
-//            let input = try String(contentsOfFile: path!, encoding: NSUTF8StringEncoding)
-//        } catch let e {
-//            Swift.print("\(e)")
-//        }
-        
-        let device = MetalDevice()
-        let z = device.sigmoid([1.0, 1.0, 1.0])
-        print(z);
-    }
+    init(){    }
     
    }
